a6858d396c282bb59a32b672cb25548acbb47026
SerhiiKhomenchuk <116754103+SerhiiKhomenchuk@users.noreply.github.com>
2023-09-10 19:12:08 +0300

    ready for inspection

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /** @format */
-
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 /** @format */
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const URI = process.env.DB_URI;
 
 const app = require("./app");
 
-app.listen(8080, () => {
-  console.log("Server running. Use our API on port: 8080");
-});
+async function run() {
+  try {
+    await mongoose.connect(URI);
+
+    console.log("Database connection successfully!");
+    app.listen(8080, () => {
+      console.log("Server running. Use our API on port: 8080");
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  // finally {
+  //   mongoose.disconnect();
+  // }
+}
+run();
